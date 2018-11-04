3164b5c60b3800823c4894cbd2c172ca0bd008d9
Sweet Ace <lakshmialeman@gmail.com>
2018-11-03 19:39:49 -0700

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Bamazon
+# Bamazon a Node.js & MySQL application
+
+The Bamazon application is an Amazon like storefront. The app will take in orders from customers and deplete stock from the store's inventory.
+
+1. The customer will be presented with a table of all the items in stock.
+2. The app will ask two questions:
+    1. Please input the id of the product you would like to buy
+    2. How many units would you like to buy
+3. If there is enough items in the inventory the total for the transaction will be presented and terminate the program.
+![bamazon app demo](bamazonApp.mov "Bamazon App Demo")
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,11 @@
+//include all npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
+
 // create the connection information for the sql database
 var connection = mysql.createConnection({
+  //localhost
   host: "localhost",
 
   // Your port; if not 3306
@@ -11,40 +14,51 @@ var connection = mysql.createConnection({
   // Your username
   user: "root",
 
-  // Your password
+  // Your password if required and the name of the database
   password: "",
   database: "bamazon"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
+  //handle errors
   if (err) throw err;
   // run the print function after the connection is made to prompt the user
   print();
 });
+
+//shows the products in stock
 function print() {
+  //initialize new table
   var table = new Table({
     head: ['ID', 'Product Name', 'Category Name', 'Price'],
     colWidths: [5, 40, 20, 12]
   });
-  // when finished prompting, insert a new item into the db with that info
+
+  // access the products in the database
   connection.query(
     "SELECT * FROM products",
     function (err, result, fields) {
+      //handles errors
       if (err) throw err;
-      //console.log(fields);
+
+      //loop through each row
       result.forEach(function (row) {
-        //console.log(row.item_id);
+        //load data onto table
         table.push(
           [row.item_id, row.product_name, row.department_name, row.price]
         );
       });
+
+      //display table on terminal
       console.log(table.toString());
+
+      //prompt the user to buy an item from the list
       placeOrder();
     }
   );
 }
-// function which prompts the user for what action they should take
+// function which prompts the user to buy an item
 function placeOrder() {
   inquirer
     .prompt([
@@ -63,7 +77,7 @@ function placeOrder() {
         type: "input",
         message: "How many units would you like to buy: ",
         validate: function (value) {
-          if (value.match(/\d+/)) {
+          if (value.match(/\d+/) && parseInt(value) > 0) {
             return true;
           }
           return false;
@@ -71,46 +85,66 @@ function placeOrder() {
       }
     ])
     .then(function (answer) {
+      //initialize the product id
       let chosenProductId = answer.productId;
-      let chosenProductToBuy = answer.buyProduct;
-      //console.log(answer);
 
-      // based on their answer, either call the bid or the post functions
-      //console.log("am i working?");
-      readProducts(chosenProductId, chosenProductToBuy);
+      //initialize the number of products to be bought
+      let chosenProductToBuy = answer.buyProduct;
+
+      // based on their answer place the order
+      reduceProductCount(chosenProductId, chosenProductToBuy);
     });
 }
-function readProducts(id, buy) {
-  //console.log("Selecting all products...\n");
+function reduceProductCount(id, buy) {
+  //access products given their id
   connection.query("SELECT * FROM products WHERE item_id= " + mysql.escape(id), function (err, res) {
+    //handles errors
     if (err) throw err;
+
+    //initialize product count from teh database
     let currentProductCount = res[0].stock_quantity;
+
+    //runs if there are products available
     if (currentProductCount >= buy) {
+      //reduce the stock size by the amount of products being bought
       currentProductCount -= buy;
-      //console.log(currentProductCount);
+      
+      //update the stock
       updateProduct(currentProductCount, id);
+
+      //initialize the price per item
+      let price = res[0].price;
+
+      //display the total for the transaction
+      total(buy, price);
     } else {
+      //display in case there is not enough stock
       console.log("Insufficient quantity!")
+
+      //display the question menu aqain to give the user a change to order another product
+      placeOrder();
     }
-    //console.log(res);
-    let price = res[0].price;
-    total( buy, price);
   });
- 
 }
+
+//update products on the database
 function updateProduct(quantity, id) {
-  //console.log("Updating all product quantities...\n");
   var query = connection.query(
     "UPDATE products SET stock_quantity=" + mysql.escape(quantity) + " WHERE item_id=" + mysql.escape(id), function (err, res) {
+      //provides confirmation the products quantities were updated
       //console.log(res.affectedRows + " products updated!\n");
     }
   );
-
-  // logs the actual query being run
-  //console.log(query.sql);
 }
-function total( chosenProductToBuy, chosenProductPrice){
+
+//shows the total for the order
+function total(chosenProductToBuy, chosenProductPrice) {
+  //placed to help readability
   console.log("-------------------------------------------------------");
+
+  //print out the total for the order
   console.log("Your total is $" + (chosenProductToBuy * chosenProductPrice).toPrecision(4));
+
+  //close connection to the database
   connection.end();
 }
